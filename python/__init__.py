b43814ee64e9dc94555694176dab67816bb3b9b1
= <luizotavio99@outlook.com>
2023-08-09 21:38:17 -0300

    estruturacao efetuada

--- python/__init__.py
+++ python/__init__.py
@@ -3,6 +3,9 @@ import time
 def incluir():
     pass
 
-for i in [1,2,3,4,5]:
-    print(f'resultado {i}')
-    
+def main():
+    for i in [1,2,3,4,5]:
+        print(f'resultado {i}')
+
+if __name__ == '__main__':
+    main()
